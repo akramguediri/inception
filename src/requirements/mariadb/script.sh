7d0d1cfb00429508564920c0485434429c9fa576
akramguediri <akramguediri2@gmail.com>
2024-07-17 21:00:40 +0200

    fix

--- src/requirements/mariadb/script.sh
+++ src/requirements/mariadb/script.sh
@@ -2,30 +2,14 @@
 
 GREEN='\033[0;32m'
 NC='\033[0m'
-
-# Generate the initialization SQL script
 echo "FLUSH PRIVILEGES;
 	CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 	CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 	GRANT ALL PRIVILEGES on \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
-	FLUSH PRIVILEGES;" >> /etc/mysql/db_init.sql
-# chmod 777 /etc/mysql/db_init.sql
-# Start MariaDB service
-echo -e "${GREEN}Starting MariaDB...${NC}"
+	FLUSH PRIVILEGES;" > /etc/mysql/db_init.sql
+
+echo -e "${GREEN}Installing MariaDB...${NC}"
 mariadb-install-db
 
-# service mariadb start
-# # Wait for MariaDB to be fully up and running
-# until mysqladmin ping --silent -u root -p"${DB_ROOT_PASSWORD}"; do
-#   echo -e "${GREEN}Waiting for MariaDB to be up...${NC}"
-#   sleep 2
-# done
-
-# Execute the initialization SQL script
-# echo -e "${GREEN}Running initialization script...${NC}"
-# mysql_upgrade -u root -p"${DB_ROOT_PASSWORD}" < /etc/mysql/db_init.sql
-
-# Start MariaDB in the foreground (required for container to remain running)
 echo -e "${GREEN}Starting MariaDB in foreground...${NC}"
 exec mariadb --user=mysql
-# exec mysqld_safe
