5a9287f6de109c0a28538aa4aeea2acaab0f4cd7
akramguediri <akramguediri2@gmail.com>
2024-07-17 18:36:24 +0200

    fix

--- src/requirements/mariadb/script.sh
+++ src/requirements/mariadb/script.sh
@@ -18,7 +18,7 @@ echo -e "${GREEN}Starting MariaDB...${NC}"
 service mariadb start
 
 # Wait for MariaDB to be fully up and running
-until mysqladmin ping --silent -u root -p"${DB_ROOT_PASSWORD}"; do
+until mysqladmin ping --silent -u root -p"a"; do
   echo -e "${GREEN}Waiting for MariaDB to be up...${NC}"
   sleep 2
 done
